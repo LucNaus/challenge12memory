f898e0bd2b7007821c64ceb11848a434dfad5463
YouriBartholomeus <92152496+YouriBartholomeus@users.noreply.github.com>
2022-06-21 09:33:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="css/stylesheet.css" rel="stylesheet">
+    <script src="js/javascript.js"></script>
+    <title>memorie</title>
+</head>
+<body>
+
+    <div id="startScreen">
+  <div>
+    <h1>de vistabenderniet memorie!</h1>
+  </div>
+  <div>
+    <p>klik start om game te starten :)</p>
+  </div>
+</div>
+
+</body>
+</html>
+
